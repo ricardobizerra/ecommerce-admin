17ae842785bebd3dd37c5d0e3006bccbe8c0bea0
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-09-18 07:29:29 -0300

    feat(main): initial setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <p>hi admin dashboard</p>
+      <Button size="sm" variant="outline">Click me</Button>
+    </>
+  )
+}
